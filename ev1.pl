63c00b8d847d7521ffedda67a7df0f22275030d0
felix-rojas <a01201946@tec.mx>
2024-02-27 11:21:34 -0600

    add ev1 base file, checks for valid var names

---
+++ ev1.pl
@@ -0,0 +1,76 @@
+% Define the DFA states
+state(q0). % initial state, no input
+state(q1). % accepting state
+state(q2). % underscore state
+state(q3). % dollar state
+state(q4). % has number state
+
+% Accepting and initial states
+accepting(q1).
+accepting(q4).
+initial(q0).
+
+% Define the alphabet
+alphabet([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,
+          'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']).
+
+% transition functions
+
+transition(q0, X, q1) :-
+    alphabet(A), % unify list to A
+    member(X, A).
+
+transition(q0, X, q2) :-
+    X == '_'.
+
+transition(q0, X, q3) :-
+    X == '$'.
+
+transition(q1, X, q1) :-
+    alphabet(A),
+    member(X, A).
+
+transition(q1, X, q2) :-
+    X == '_'.
+
+transition(q1, X, q4) :-
+    number(X).
+
+transition(q4, X, q4) :-
+    number(X).
+
+transition(q4, X, q1) :-
+    alphabet(A),
+    member(X, A).
+
+transition(q4, X, q2) :-
+    X == '_'.
+
+transition(q2, X, q2) :-
+    X == '_'.
+
+transition(q2, X, q1) :-
+    alphabet(A),
+    member(X, A).
+
+transition(q2, X, q4) :-
+    number(X).
+
+transition(q3, X, q1) :-
+    alphabet(A),
+    member(X, A).
+
+% Base case, empty list and state accepting
+accepts(State, []) :-
+    accepting(State).
+
+% iterate each head on string
+accepts(State, [H|T]) :-
+    transition(State, H, NextState),
+    accepts(NextState, T).
+
+% Entry point for checking input against the DFA
+check(Input) :-
+    initial(InitialState),
+    accepts(InitialState, Input).
+
