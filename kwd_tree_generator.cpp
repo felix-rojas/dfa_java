e76760b648d8b7bf955c0a2a73ed95bf5498b508
felix-rojas <a01201946@tec.mx>
2024-03-18 11:53:25 -0600

    add automatic keyword transitions generator

--- kwd_tree_generator.cpp
+++ kwd_tree_generator.cpp
@@ -1,18 +1,94 @@
+#include <cctype>
 #include <fstream>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 
-using std::string; 
-using std::vector; 
-using std::ifstream; 
-using std::cout; 
-using std::endl; 
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::string;
+using std::vector;
 
-void generate_states(const string &keyword){
-    for (unsigned int i = 0; i < keyword.length(); i++){
-        cout << "state(" << keyword << i << ")." << endl;
+vector<string> generate_states(const string &keyword) {
+  vector<string> states;
+  states.push_back(keyword);
+  for (unsigned int i = 0; i < keyword.length(); i++) {
+    string temp = keyword + std::to_string(i);
+    states.push_back(temp);
+  }
+  return states;
+}
+
+void generate_transitions(vector<string> &states) {
+  cout << "% -------------------" << endl;
+  cout << "% Keyword "<< states[0] <<" Transitions" << endl;
+  cout << "% -------------------" << endl;
+  
+  cout << "transition(keyword_check,X," + states[1] + "):-" << endl;
+    cout << "\tX==\'" << states[0][0] << "\';" << endl;
+    cout << "\tX==\'" << char(std::toupper(states[0][0])) << "\'." << endl;
+
+  for (unsigned int i = 1; i < states.size() - 1; i++) {
+    string temp1 = states[i];
+    string temp2 = states[i + 1];
+    char state_lettters = states[0][i];
+    cout << "transition(" + temp1 + ",X," + temp2 + "):-" << endl;
+    cout << "\tX==\'" << state_lettters << "\';" << endl;
+    cout << "\tX==\'" << char(std::toupper(state_lettters)) << "\'." << endl;
+  }
+  cout << "% ------------------" << endl;
+  cout << "% Transitions to q1" << endl;
+  cout << "% ------------------" << endl;
+  for (unsigned int i = 1; i < states.size(); i++) {
+    string temp1 = states[i];
+    char state_lettters = states[0][i];
+    cout << "transition(" + temp1 + ",X," + "q1" + "):-" << endl;
+    if (i != states.size()-1) {
+    cout << "\tX\\=\'" << state_lettters << "\'," << endl;
+    cout << "\tX\\=\'" << char(std::toupper(state_lettters)) << "\'," << endl;
+    cout << "\tchar_type(X,alpha)" << "." << endl;
+    } else {
+      cout << "\tchar_type(X,alpha)" << "." << endl;
     }
+  }
+  cout << "% -----------------" << endl;
+  cout << "% Transitions to q3" << endl;
+  cout << "% -----------------" << endl;
+  for (unsigned int i = 1; i < states.size(); i++) {
+    string temp1 = states[i];
+    cout << "transition(" + temp1 + ",X," + "q3" + "):-";
+    cout << "\tX==\'$\'"
+         << "." << endl;
+  }
+  cout << "% -----------------" << endl;
+  cout << "% Transitions to q4" << endl;
+  cout << "% -----------------" << endl;
+  for (unsigned int i = 1; i < states.size(); i++) {
+    string temp1 = states[i];
+    char state_lettters = states[0][i];
+    cout << "transition(" + temp1 + ",X," + "q4" + "):-" << endl;
+    if (i != states.size()-1) {
+    cout << "\tX\\=\'" << state_lettters << "\'," << endl;
+    cout << "\tX\\=\'" << char(std::toupper(state_lettters)) << "\'," << endl;
+    cout << "\tchar_type(X,digit)"
+         << "." << endl;
+    } else {
+      cout << "\tchar_type(X,alpha)" << "." << endl;
+    }
+  }
+  cout << "% -----------------" << endl;
+  cout << "% Transitions to q5" << endl;
+  cout << "% -----------------" << endl;
+  for (unsigned int i = 1; i < states.size(); i++) {
+    string temp1 = states[i];
+    char state_lettters = states[0][i - 1];
+    cout << "transition(" + temp1 + ",X," + "q5" + "):-";
+    cout << "\tX==\'_\'"
+         << "." << endl;
+  }
+ 
 }
 
 int main() {
@@ -24,12 +100,13 @@ int main() {
       kwds_vec.push_back(line);
     }
     file.close();
-  }
-  else {
+  } else {
     cout << "No keyword list found." << endl;
   }
+
   for (auto keyword : kwds_vec) {
-    generate_states(keyword);
+    vector<string> kwd_states = generate_states(keyword);
+    generate_transitions(kwd_states);
   }
 
   return 0;
