7d304787b8da11b7f67535844491b331c9f9e86b
felix-rojas <a01201946@tec.mx>
2024-06-04 08:54:35 -0600

    removed PEP8 violations, fix traceback error

--- regex_test.py
+++ regex_test.py
@@ -1,6 +1,7 @@
 import re
 import os
 
+
 def parse_identifier(string):
     pattern = r'^[a-zA-Z$]|[_]+[a-zA-Z\d_$]+$'
     p = re.compile(pattern)
@@ -10,6 +11,7 @@ def parse_identifier(string):
     else:
         print(f"'{string}' NOT a valid identifier")
 
+
 def parse_kwd(string):
     pattern = r'(\bmain\b)|(\bvoid\b)|(\bint\b)|(\bprivate\b)|(\bpublic\b)'
     match = re.match(pattern, string)
@@ -17,8 +19,9 @@ def parse_kwd(string):
         print(f"'{string}' is a RESERVED KEYWORD")
 
 
-valid_strings = ["a","$astrid", "$$aa", "a$A$a$_", "b$", "A$a", "$as1111", "$____$$$$", "$$$as$$", "$_", "$", "a_", "_a", "_9", "_$", "__","_"," a",]
-invalid_ids = ["12","1_2","1$2","1 2"," "," _","_ _","_","1main","2mark","2_mark","2__mark","2_$"," a"]
+valid_strings = ["a", "$astrid", "$$aa", "a$A$a$_", "b$", "A$a", "$as1111", "$____$$$$", "$$$as$$", "$_", "$", "a_",
+                 "_a", "_9", "_$", "__", "_", " a", ]
+invalid_ids = ["12", "1_2", "1$2", "1 2", " ", " _", "_ _", "_", "1main", "2mark", "2_mark", "2__mark", "2_$", " a"]
 
 for s in valid_strings:
     parse_identifier(s)
@@ -26,11 +29,12 @@ for s in valid_strings:
 for s in invalid_ids:
     parse_identifier(s)
 
-file_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = "\kwds.txt"
+file_dir = os.path.dirname(os.path.abspath(__name__))
+file_path = "kwds.txt"
+path_to_file = os.path.join(file_dir, file_path)
 
-if os.path.exists(file_dir+file_path):
-    with open(file_dir+file_path, "r") as file:
+if os.path.exists(path_to_file):
+    with open(path_to_file, "r") as file:
         java_keywords = [line.rstrip("\n") for line in file]
 else:
     print(f"{file_path} not found")
